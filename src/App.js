d82c5eae65562f8881335b5bdc59135ca6b9de76
Jicheol Oh <jicheol@gmail.com>
2019-06-12 12:19:38 -0700

    PhoneInfo: 각 전화번호 정보를 보여주는 컴포넌트입니다.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,45 @@ import PhoneForm from './components/PhoneForm';
 import './App.css';
 
 class App extends Component {
+  // id 값은 각 데이터를 식별하기 위함입니다. 그리고 이 값은 데이터를 추가할때마다 숫자를 1씩 더해주겠습니다.
+  // id 값의 경우에는, 컴포넌트의 일반 클래스 내부 변수로서 선언해주었습니다.
+  // 컴포넌트 내부에서 필요한 값 중에서, 렌더링 되는것과 상관이 없는것들은 굳이 state 에 넣어줄 필요가 없습니다.
+  id = 2
+
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+
   // 이제 state 안에 있는 값들을 부모 컴포넌트에게 전달해줄 차례입니다.
   // 이러한 상황에는, 부모 컴포넌트에서 메소드를 만들고,
   // 이 메소드를 자식에게 전달한 다음에 자식 내부에서 호출하는 방식을 사용합니다.
   handleCreate = (data) => {
-    console.log(data);
+    // console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
   }
   render() {
+    const { information } = this.state;
     return (
         <div>
           <PhoneForm
               onCreate={this.handleCreate}
           />
+          {/*render 함수에서는 information 값을 문자열로 변환하여 보여주었습니다.*/}
+          {JSON.stringify(information)}
         </div>
     );
   }
---
+++ src/components/PhoneInfo.js
@@ -0,0 +1,40 @@
+// file: src/components/PhoneInfo.js
+import React, { Component } from 'react';
+
+
+// 우리는 info 라는 객체를 props 로 받아와서 렌더링 해줄것입니다.
+// 그런데, 우리가 실수로 info 값을 전달해주는것을 까먹게 된다면 컴포넌트가 크래쉬 될 것입니다.
+// info 가 undefined 일 때에는 비구조화 할당을 통해 내부의 값을 받아올 수 없기 때문입니다.
+
+// 그렇기 때문에 defaultProps 를 통하여 info 의 기본값을 설정해주었습니다.
+
+class PhoneInfo extends Component {
+    // 가끔씩은 실수로 props 를 빠트려먹을때가 있습니다. 혹은, 특정 상황에 props 를 일부러 비워야 할 때도 있구요.
+    // 그러한 경우에, props 의 기본값을 설정해줄 수 있는데요, 그것이 바로 defaultProps 입니다.
+    static defaultProps = {
+        info: {
+            name: '이름',
+            phone: '010-0000-0000',
+            id: 0
+        }
+    }
+
+    render() {
+        const style = {
+            border: '1px solid black',
+            padding: '8px',
+            margin: '8px'
+        };
+
+        const { name, phone, id } = this.props.info;
+
+        return (
+            <div style={style}>
+                <div><b>{name}</b></div>
+                <div>{phone}</div>
+            </div>
+        )
+    }
+}
+
+export default PhoneInfo;
